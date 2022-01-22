7b6defc8a9f1c9f140e4ab2eab820d1897ff8b75
Cesar Riano <cesarrianopinto@gmail.com>
2022-01-22 07:53:59 -0500

    three commit

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 importScripts('js/sw_aux.js');
 
 //creacion de caches
-const STATIC_CACHE="static-v2";
+const STATIC_CACHE="static-v3";
 const DYNAMIC_CACHE="dynamic-v1";
 const INMUTABLE_CACHE="inmutable-v1";
 
